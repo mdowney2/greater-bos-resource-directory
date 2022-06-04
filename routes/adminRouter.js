b92623758551158ac4cad621b6a36df507ad00c0
mdowney2 <michaela.r.downey@gmail.com>
2022-06-04 11:37:54 -0400

    update

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 const issueController = require('../controllers/issueController');
 const resourceController = require('../controllers/resourceController');
+const siteController = require('../controllers/siteController');
 
 router.route('/')
     .get(adminController.admin)
+    .get(siteController.login);
 
 router.route('/issues')
     .get(adminController.admin_showIssues)
