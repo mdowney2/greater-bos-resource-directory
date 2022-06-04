2577921e05165b0f8b86d8e82501ff58315b7b70
mdowney2 <michaela.r.downey@gmail.com>
2022-06-04 10:52:35 -0400

    update

--- models/resourcesModel.js
+++ models/resourcesModel.js
@@ -4,25 +4,25 @@ const {Schema} = mongoose;
 const resourceSchema = new Schema({
     resourceName: {
         type: String,
-        required: [true, 'The resource name is required.'],
-        mingLength: [1, "Minimum length is 1 character."]
+        // required: [true, 'The resource name is required.'],
+        // mingLength: [1, "Minimum length is 1 character."]
     },
     resourceType: {
         type: Array,
-        required: [true, 'Please select resource type.']
+        // required: [true, 'Please select resource type.']
     },
     issue: {
         type: Array,
-        required: [true, 'Applicable issue(s) required.']
+        // required: [true, 'Applicable issue(s) required.']
     },
     resourceLink: {
         type: String,
-        required: [true, 'Please include a link to the resource.']
+        // required: [true, 'Please include a link to the resource.']
     },
     description: {
         type: String,
-        required: [true, 'Please include a brief description.'],
-        minLength: [1, 'Minimum length 1 character.']
+        // required: [true, 'Please include a brief description.'],
+        // minLength: [1, 'Minimum length 1 character.']
     }
 });
 
